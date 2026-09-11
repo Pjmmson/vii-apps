ef922f97f106dfba196cc37527e3c7e90345d812
phimmasone.thmvs <phimmasone.thmvs@gmail.com>
2026-09-11 08:58:41 +0700

    fixed api connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,25 @@
 import { useEffect, useState } from "react";
 import api from "../services/api/baseAPI";
 import enpoints from "../services/api/enpoints";
-import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("Testing API connection ...");
 
-  // useEffect(() => {
-  //   console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
-  //   console.log("Axios baseURL:", api.defaults.baseURL);
-  //   const testConnection = async () => {
-  //     try {
-  //       // Just pass "test-connection" without any slashes
-  //       const response = await api.get(enpoints.testConnection.base); 
-
-  //       console.log("Response:", response.data);
-  //       setMessage(response.data.message);
-  //     } catch (error) {
-  //       console.error("Connection failed:", error);
-  //       setMessage("Connection failed.");
-  //     }
-  //   };
-  //   testConnection();
-  // }, []);
   useEffect(() => {
-    const controller = new AbortController();
-    axios.get("http://127.0.0.1:8000/api/test-connection",{timeout:10000,
-      signal: controller.signal
-    }).then(response => {console.log(response.data.message);
-      setMessage("Connection success !");
-    }).catch(error => {
-      if (axios.isCancel(error)) {
-        console.log("Request canceled intentionally during cleanup.");
-      } else {
-        console.error("Connection failed: ", error);
-        setMessage("Failed connection ...");
+    console.log("URL: ",import.meta.env.VITE_API_URL+enpoints.testConnection.base);
+    const url = import.meta.env.VITE_API_URL+enpoints.testConnection.base;
+    const testConnection = async (url) => {
+      try {
+        const response = await api.get(url); 
+        console.log("Response:", response.data);
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error("Connection failed:", error);
+        setMessage("Connection failed.");
       }
-    });
-    return () => {
-      controller.abort();
     };
-  },[]);
+    testConnection(url);
+  }, []);
 
   return (
   <div>
